b27c578e184c425258ad8eeace63352c03e9f2d3
Yaov05 <ortizalejandro551199@gmail.com>
2021-07-01 13:16:16 -0500

    correcion del dos

--- inicio.go
+++ inicio.go
@@ -1,7 +1,7 @@
 package numerosaletras
 
 var (
-	unidades     = [...]string{"", "un", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"}
+	unidades     = [...]string{"", "dos", "un", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"}
 	primerosDiez = [...]string{"diez", "once", "doce", "trece", "catorce", "quince", "dieciseis",
 		"diecisiete", "dieciocho", "diecinueve"}
 )
