66da6a280a3ca72b2daec13f6686653ffb59beec
Yaov05 <ortizalejandro551199@gmail.com>
2021-07-01 13:22:40 -0500

    arreglado dos

--- inicio.go
+++ inicio.go
@@ -1,11 +1,7 @@
 package numerosaletras
 
 var (
-<<<<<<< HEAD
-	unidades     = [...]string{"", "dos", "un", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"}
-=======
 	unidades     = [...]string{"", "un", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"}
->>>>>>> df36b52ba87e0f813da88baffe99b466573a710a
 	primerosDiez = [...]string{"diez", "once", "doce", "trece", "catorce", "quince", "dieciseis",
 		"diecisiete", "dieciocho", "diecinueve"}
 )
